8248f3f19cc031cf64c6c7c65bfe77a5fc6e689c
ystc1247 <ystc1247@khu.ac.kr>
2026-04-04 10:46:21 +0900

    Suppress nested VirtualizedList warning for demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { LogBox } from "react-native";
 import { Navigation } from './navigation';
 import { AuthProvider } from './auth';
 import { LocaleProvider } from "./locale";
+
+LogBox.ignoreLogs([
+  "VirtualizedLists should never be nested inside plain ScrollViews",
+]);
 
 export default function App() {
   return (
